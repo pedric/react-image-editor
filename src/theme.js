ed284e2b8db1085c78f82c9db1426cc244141af8
Fredrik <epost.larsson@gmail.com>
2021-04-10 23:10:08 +0200

    clean up + add html2canvas, file-saver

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  color: {
+    primary: "tomato",
+    secondary: "blue",
+    backgroundDark: "#333",
+    backgroundmedium: "#888",
+  },
+  type: {
+    primaryFont: "sans-serif",
+  },
+};
